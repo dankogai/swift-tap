ca1c5950e8304b96bdb9adda16382cd9c983e455
Dan Kogai <dankogai+github@gmail.com>
2020-09-09 23:13:44 +0900

    Revert "fix linux part of scripts/run-tests.sh"
    This reverts commit bc534a74b33502cbf4942a73edd908e17c345952.

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -3,22 +3,6 @@
 set -ev
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    sudo apt-get -y install \
-          binutils \
-          git \
-          gnupg2 \
-          libc6-dev \
-          libcurl4 \
-          libedit2 \
-          libgcc-9-dev \
-          libpython2.7 \
-          libsqlite3-0 \
-          libstdc++-9-dev \
-          libxml2 \
-          libz3-dev \
-          pkg-config \
-          tzdata \
-          zlib1g-dev
     DIR="$(pwd)"
     cd ..
     export SWIFT_VERSION=swift-5.2.5
