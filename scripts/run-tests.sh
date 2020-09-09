68c60df4468c4b67a48d7bf085d2091cf6513959
Dan Kogai <dankogai+github@gmail.com>
2020-09-09 23:14:24 +0900

    Revert "update linux part of scripts/run-tests.sh"
    This reverts commit d41fc05ae69e6fc822d091ffbe91c5ac7888a553.

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -5,11 +5,10 @@ set -ev
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     DIR="$(pwd)"
     cd ..
-    export SWIFT_VERSION=swift-5.2.5
-    export SWIFT_RELEASE=${SWIFT_VERSION}-RELEASE
-    export UBUNTU_VERSION=ubuntu20.04
-    wget https://swift.org/builds/${SWIFT_VERSION}-release/ubuntu2004/${SWIFT_RELEASE}/${SWIFT_RELEASE}-${UBUNTU_VERSION}.tar.gz
-    tar xzf ${SWIFT_RELEASE}-${UBUNTU_VERSION}.tar.gz
-    export PATH="${PWD}/${SWIFT_RELEASE}-${UBUNTU_VERSION}/usr/bin:${PATH}"
+    export SWIFT_VERSION=swift-4.1.1-RELEASE
+    export UBUNTU_VERSION=ubuntu16.04
+    wget https://swift.org/builds/swift-4.1.1-release/ubuntu1604/${SWIFT_VERSION}/${SWIFT_VERSION}-${UBUNTU_VERSION}.tar.gz
+    tar xzf ${SWIFT_VERSION}-${UBUNTU_VERSION}.tar.gz
+    export PATH="${PWD}/${SWIFT_VERSION}-${UBUNTU_VERSION}/usr/bin:${PATH}"
     cd "$DIR"
 fi
